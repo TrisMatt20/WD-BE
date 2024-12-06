4f6ca994012089d6f1aed8bdc1384fc3bfc4e094
TrisMatt20 <tmtmatencio@gmail.com>
2024-12-06 18:09:57 +0800

    Integrated Database for Home

--- activities/A08/home.php
+++ activities/A08/home.php
@@ -1,3 +1,24 @@
+<?php
+include("connect.php");
+include("shared/classes.php");
+
+$islandList = array();
+
+$islandQuery = "SELECT * FROM islandsofpersonality";
+$islandResults = executeQuery($islandQuery);
+
+while ($islandRow = mysqli_fetch_assoc($islandResults)) {
+    $island = new Island(
+        $islandRow['name'],
+        $islandRow['shortDescription'],
+        $islandRow['image'],
+        $islandRow['islandOfPersonalityID']
+    );
+
+    array_push($islandList, $island);
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -24,7 +45,8 @@
         <div class="row">
             <div class="col">
                 <h1 class="title display-4">Welcome to TM Lord's Headquarters!</h1>
-                <h3 class="title-desc mt-5">You've arrived at the heart of my mind, where every thought, emotion, and dream
+                <h3 class="title-desc mt-5">You've arrived at the heart of my mind, where every thought, emotion, and
+                    dream
                     is brought to life.</h3>
                 <img class="homePic img-fluid animate-up" src="assets/img/homePic.png" alt="homePic">
             </div>
@@ -35,98 +57,18 @@
         <div class="row">
             <div class="col">
                 <div class="navigate display-5 animate-up">Navigate through my personality islands!</div>
-            </div>
-            <div class="row mt-5">
-                <div class="col-12 col-md-6">
-                    <img class="friendshipIsland img-fluid" src="assets/img/Friendship Island.png"
-                        alt="Friendship Island">
+                <div class="row mt-5">
+                    <?php
+                    foreach ($islandList as $islandItem) {
+                        echo $islandItem->createIsland();
+                    }
+                    ?>
+                </div>
 
-                    <h3 class="islandName">
-                        Bonding Bay
-                    </h3>
-                    <h5 class="p1 animate-up">
-                        This island is the foundation of my strongest bonds. Here reside laughter, shared experiences,
-                        and countless memories forged with friends—adventures that have shaped our connection. It's a
-                        place where the spirit of friendship thrives, reminding me of the special people by my side.
-                    </h5>
-                    <div class="d-flex justify-content-center">
-                        <a href="../../loading page/loadingPage.php">
-                            <button class="visit button-64 animate-up" role="button"><span class="text">
-                                    Visit Island
-                                </span>
-                            </button>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <img class="pixelPeak img-fluid" src="assets/img/Pixel Peak.png" alt="Pixel Peak">
-                    <h3 class="islandName2">
-                        Pixel Peak
-                    </h3>
-                    <h5 class="p2 animate-up">
-                        Pixel Peak island embodies my gaming experiences, showcasing my passion for immersive worlds,
-                        strategic thinking, and creative exploration. It's where my favorite gaming moments,
-                        achievements, and adventures come to life, reminding me of the thrill and joy in every
-                        challenge.
-                    </h5>
-                    <div class="d-flex justify-content-center">
-                        <a href="../../loading page/loadingPage.php">
-                            <button class="visit button-64 animate-up" role="button">
-                                <span class="text">
-                                    Visit Island
-                                </span>
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt-5">
-                <div class="col-12 col-md-6">
-                    <img class="hoopHaven img-fluid" src="assets/img/Hoop Haven.png" alt="Hoop Haven">
-                    <h3 class="islandName3 animate-up">
-                        Hoop Haven
-                    </h3>
-                    <h5 class="p3 animate-up">
-                        This island represents my basketball experiences. It's a place where my love for the sport, the
-                        thrill of competition, and the lessons of teamwork and determination come together, driving me
-                        to excel both on and off the court, pushing me to always strive for greatness.
-                    </h5>
-                    <div class="d-flex justify-content-center">
-                        <a href="../../loading page/loadingPage.php">
-                            <button class="visit button-64 animate-up" role="button">
-                                <span class="text">Visit Island
-                                </span>
-                            </button>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <img class="technoSphere img-fluid" src="assets/img/TechnoSphere.png" alt="TechnoSphere">
-                    <h3 class="islandName4 animate-up">
-                        TechnoSphere
-                    </h3>
-                    <h5 class="p4 animate-up">
-                        Technosphere island holds my knowledge and experiences in the technical world. It's a hub where
-                        my passion for innovation, problem-solving, and continuous learning in technology thrives,
-                        driving me to explore and create in the ever-evolving digital landscape.
-                    </h5>
-                    <div class="d-flex justify-content-center">
-                        <a href="../../loading page/loadingPage.php">
-                            <button class="visit button-64 animate-up" role="button">
-                                <span class="text">Visit Island
-                                </span>
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <script src="assets/js/animate-up.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+                <script src="assets/js/animate-up.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+                    crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ activities/A08/shared/classes.php
@@ -0,0 +1,38 @@
+<?php
+class Island
+{
+    public $name;
+    public $description;
+    public $image;
+    public $islandOfPersonalityID;
+
+    public function __construct($name, $description, $image, $islandOfPersonalityID)
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->image = $image;
+        $this->islandOfPersonalityID = $islandOfPersonalityID;
+    }
+
+    public function createIsland()
+    {
+        return "
+        <div class='col-12 col-md-6'>
+            <img class='islandImage img-fluid' src='assets/img/" . $this->image . "' alt='" . $this->name . "'>
+            <h3 class='islandName'>
+                " . $this->name . "
+            </h3>
+            <h5 class='p1 animate-up'>
+                " . $this->description . "
+            </h5>
+            <div class='d-flex justify-content-center'>
+                <a href='view.php?id=" . $this->islandOfPersonalityID . "'>
+                    <button class='visit button-64 animate-up' role='button'>
+                        <span class='text'>Visit Island</span>
+                    </button>
+                </a>
+            </div>
+        </div>";
+    }
+}
+?>
